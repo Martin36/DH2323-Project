9b07024871c8e50491b4077a84faac6bda7d0f19
Martin <martin.funkquist@gmail.com>
2016-09-03 12:57:40 +0200

    Imported model of island to Unity.

---
+++ Tropical Island/Assets/Screenshot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshot : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	//	Application.CaptureScreenshot("C:/Users/martin/Documents/git/DH2323-Project/Tropical Island/Assets/IslandUnity01.png");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
